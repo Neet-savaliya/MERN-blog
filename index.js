8405b7188194f72d9c730a790d911bc05d3482a1
neet <neetsavaliya03@.com>
2024-04-13 21:39:30 +0530

    Connected to database

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
-mongoose.connect("mongodb+srv://node_complete:2801@cluster0.5zm0eog.mongodb.net/mern-blog")
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(3000, () => {
-    console.log("running on 3000");
-});
+mongoose
+    .connect(process.env.DATABASE_CONNECTION)
+    .then((result) => {
+        app.listen(3000, () => {
+            console.log("running on 3000");
+        });
+    })
+    .catch((err) => console.log(err));
